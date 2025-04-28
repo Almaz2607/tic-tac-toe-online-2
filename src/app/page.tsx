1c9f546f86f738fd4a04761b0ad41e1f0a35eb19
Almaz Kubakaev <almazkubakaev2607@gmail.com>
2025-04-28 19:33:22 +0600

    feat: prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,24 @@ import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
 import { Card, CardTitle } from "@/shared/ui/card";
 
-
 export default async function Home() {
-  const games = await prisma.game.findMany()
+  const games = await prisma.game.findMany();
 
-  console.log(games)
+  console.log(games);
 
   return (
-   <div>
-    <Button size='lg' variant='destructive'>Hellow!</Button>
+    <div>
+      <Button size="lg" variant="destructive">
+        Hellow!
+      </Button>
 
-    {games.map(game=> {
-      return (
-        <Card key={game.id}>
-          <CardTitle>{game.name}</CardTitle>
-        </Card>
-      )
-    })}
-   </div>
+      {games.map((game) => {
+        return (
+          <Card key={game.id}>
+            <CardTitle>{game.name}</CardTitle>
+          </Card>
+        );
+      })}
+    </div>
   );
 }
