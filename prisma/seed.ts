7a8b09afea439447bd6470834673c00e48a2032c
Almaz Kubakaev <almazkubakaev2607@gmail.com>
2025-09-04 17:17:21 +0600

    feat: game model, output list of games

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,14 +3,34 @@ import { PrismaClient } from "@/generated/prisma";
 const prisma = new PrismaClient();
 
 async function main() {
+  const user = await prisma.user.create({
+    data: { login: "user", passwordHash: "faagsafaefaf", rating: 1000 },
+  });
+
+  const user2 = await prisma.user.create({
+    data: { login: "user 2", passwordHash: "faagsafsfsaefaf", rating: 700 },
+  });
+
   await prisma.game.create({
     data: {
-      name: "game-1",
+      field: Array(9).fill(null),
+      status: "idle",
+      players: {
+        connect: {
+          id: user.id,
+        },
+      },
     },
   });
   await prisma.game.create({
     data: {
-      name: "game-2",
+      field: Array(9).fill(null),
+      status: "idle",
+      players: {
+        connect: {
+          id: user2.id,
+        },
+      },
     },
   });
 }
