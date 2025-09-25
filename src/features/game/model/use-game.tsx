8648da5da5922929ebb311e1c4e80cd50db11e44
Almaz Kubakaev <almazkubakaev2607@gmail.com>
2025-09-25 12:47:10 +0600

    feat: logic of the move

--- src/features/game/model/use-game.tsx
+++ src/features/game/model/use-game.tsx
@@ -1,15 +1,27 @@
+import { useTransition } from "react";
 import { GameDomain } from "@/entities/game";
 import { GameId } from "@/kernel/ids";
 import { routes } from "@/kernel/routes";
 import { useEventSource } from "@/shared/lib/sse/client";
+import { gameStepAction } from "../actions/game-step";
 
 export function useGame(gameId: GameId) {
   const { dataStream, isPending } = useEventSource<GameDomain.GameEntity>(
     routes.gameStream(gameId),
   );
 
+  const [isPendingTransition, startTransition] = useTransition();
+
+  const step = (index: number) => {
+    startTransition(async () => {
+      await gameStepAction({ gameId, index });
+    });
+  };
+
   return {
     game: dataStream,
+    step,
     isPending,
+    isStepPending: isPendingTransition,
   };
 }
--- src/features/game/ui/status.tsx
+++ src/features/game/ui/status.tsx
@@ -3,18 +3,18 @@ import { GameDomain } from "@/entities/game";
 export function GameStatus({ game }: { game: GameDomain.GameEntity }) {
   switch (game.status) {
     case "idle": {
-      return <div>Ожидание игрока</div>;
+      return <div className="text-lg">Ожидание игрока</div>;
     }
     case "inProgress": {
-      const currentSymbol = GameDomain.getGameCurrentStep(game);
-      return <div>Ход: {currentSymbol}</div>;
+      const currentSymbol = GameDomain.getGameCurrentSymbol(game);
+      return <div className="text-lg">Ход: {currentSymbol}</div>;
     }
     case "gameOver": {
-      const currentSymbol = GameDomain.getGameCurrentStep(game);
-      return <div>Победитель: {currentSymbol}</div>;
+      const currentSymbol = GameDomain.getPlayerSymbol(game.winner, game);
+      return <div className="text-lg">Победитель: {currentSymbol}</div>;
     }
     case "gameOverDraw": {
-      return <div>Ничья</div>;
+      return <div className="text-lg">Ничья</div>;
     }
   }
 }
